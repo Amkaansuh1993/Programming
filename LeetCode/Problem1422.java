60a49445d61d99f559d06a3e081c61d655611472
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2023-12-22 14:06:23 +0530

    Dec22nd

---
+++ LeetCode/Problem1422.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * 1422
+ */
+public class Problem1422 {
+    public static void main(String[] args) {
+        int result=maxScore("1001001");
+        System.out.println(result);
+    }
+
+    public static int maxScore(String s) {
+        char[] c=s.toCharArray();
+        int lres=0,rres=0,lsum=0,rsum=0,fin=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            rres=0;
+            lres=0;
+            for ( int j=i+1;j<s.length();j++)
+            {
+                    if( c[j] == '1') 
+                        rres++;
+            } rsum=rres;
+            for ( int k=0;k<=i;k++)
+            {
+                if(c[k] == '0')
+                    lres++;
+            } lsum=lres;
+            if ( fin < lsum+rsum )
+                fin=lsum+rsum;
+        }
+
+        return fin;
+    }
+    
+}
