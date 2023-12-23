bac6ca71e6e793d4e23014b2d72f9c6ff0c7da1a
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2023-12-23 10:15:54 +0530

    Dec23rd Update

---
+++ LeetCode/DailyChallenges/Problem1.java
@@ -0,0 +1,28 @@
+package LeetCode.DailyChallenges;
+
+/**
+ * Problem1
+ */
+public class Problem1 {
+public static void main(String[] args) {
+    int[] nums={2,7,11,15};
+    int target=9;
+    int[] Result=twoSum(nums,target);
+    for(int i=0;i<Result.length;i++)
+    System.out.println(Result[i]);
+}
+public static int[] twoSum(int[] nums, int target) {
+    for ( int i=0; i<nums.length-1; i++ )
+    {
+        for ( int j=i+1; j<nums.length; j++ )
+        {
+                if ( target == nums[i]+nums[j] )
+                {
+                    int[] res={i,j};
+                    return res;
+                }
+        }
+    }
+        return nums;
+}
+}
