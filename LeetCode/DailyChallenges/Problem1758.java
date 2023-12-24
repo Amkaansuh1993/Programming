5782c5eaac72ed0f6f85c63da94c0612990ba836
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2023-12-24 09:00:07 +0530

    Dec24th Upload

---
+++ LeetCode/DailyChallenges/Problem1758.java
@@ -0,0 +1,42 @@
+package LeetCode.DailyChallenges;
+
+public class Problem1758 {
+    public static void main(String[] args) {
+        int Result=minOperations("10010100");
+        System.out.println(Result);
+    }
+
+    public static int minOperations(String s) {
+        char[] c=s.toCharArray();
+        int counter=0;
+        int size=s.length()-1;
+        for(int i=0;i<size;i++)
+        {
+            if( c[i] == '0' )
+            {if( c[i+1] != '1' ){c[i+1] = '1';counter++;}}
+            else 
+            {if( c[i+1] != '0' ){c[i+1] = '0';counter++;}}
+        }
+        c=s.toCharArray();
+        int s_counter=0;
+        if(c[0] == '0')
+            {c[0]='1';s_counter++;} 
+            else {c[0]='0';s_counter++;}
+        
+        for(int i=0;i<size;i++)
+        {
+            if( c[i] == '0' )
+            {if( c[i+1] != '1' ){c[i+1] = '1';s_counter++;}}
+            else 
+            {if( c[i+1] != '0' ){c[i+1] = '0';s_counter++;}}
+        }
+    if (counter >= s_counter)
+    {
+        return s_counter;
+    }else
+    {
+        return counter;
+    }
+        
+    }
+}
