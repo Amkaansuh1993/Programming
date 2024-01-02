eca647ff679e0d347658c2e2341adb672259fc86
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2024-01-02 08:37:53 +0530

    Jan2nd update

---
+++ LeetCode/DailyChallenges/Problem2610.java
@@ -0,0 +1,45 @@
+package LeetCode.DailyChallenges;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Problem2610
+ */
+public class Problem2610 {
+public static void main(String[] args) {
+    int[] nums={1,3,4,1,2,3,1};
+    List<List<Integer>> Res=findMatrix(nums);
+    System.out.print(Res);
+}
+    public static List<List<Integer>> findMatrix(int[] v) {
+        Map<Integer, Integer> um = new HashMap<>();
+        for (int i : v) {
+            um.put(i, um.getOrDefault(i, 0) + 1);
+        }
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        while (!um.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            List<Integer> toErase = new ArrayList<>();
+            for (Map.Entry<Integer, Integer> entry : um.entrySet()) {
+                int f = entry.getKey();
+                int s = entry.getValue();
+                temp.add(f);
+                s--;
+                if (s == 0) {
+                    toErase.add(f);
+                }
+                um.put(f, s);
+            }
+            ans.add(temp);
+            for (int i : toErase) {
+                um.remove(i);
+            }
+        }
+        return ans;
+        
+    }
+}
