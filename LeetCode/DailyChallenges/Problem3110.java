95b69f10303ed7bdd73262f81f7dcb3b85fae4c1
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2024-06-19 09:54:17 +0530

    UpdateJun19

---
+++ LeetCode/DailyChallenges/Problem3110.java
@@ -0,0 +1,37 @@
+package LeetCode.DailyChallenges;
+
+import java.util.Arrays;
+
+/**
+ * Problem3110
+ */
+public class Problem3110 {
+
+    public static void main(String[] args) {
+        String S = "hello";
+        int Res = scoreOfString(S);
+        System.out.println(Res);
+    }
+
+    public static int scoreOfString(String S) {
+        char[] c = S.toCharArray();
+        int Sum=0,tempres=0;
+        for ( int i=0; i < S.length()-1 ; i++ )
+        {
+            // int temp1 = (int)c[i];
+            // int temp2 = (int)c[i+1];
+            // if (temp2>temp1)
+            // { tempres=temp2-temp1; } 
+            // else 
+            // { tempres=temp1-temp2; } 
+            if ((int)c[i+1]>(int)c[i])
+            { tempres=(int)c[i+1]-(int)c[i]; } 
+            else 
+            { tempres=(int)c[i]-(int)c[i+1]; } 
+
+            Sum=Sum+tempres;
+        }
+        
+        return Sum;
+    }
+}
