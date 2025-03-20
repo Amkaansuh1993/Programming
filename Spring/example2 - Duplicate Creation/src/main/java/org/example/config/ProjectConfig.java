cbcf6cdb4c73fcff3daa0262d16a4f3b1a38ecb4
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2025-03-21 00:29:26 +0530

    Update Mar21

---
+++ Spring/example2 - Duplicate Creation/src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,39 @@
+package org.example.config;
+
+import org.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+    
+    @Bean
+    Vehicle vehicle(){
+        var veh = new Vehicle();
+        veh.setName("Audi8");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle2(){
+        var veh = new Vehicle();
+        veh.setName("HondaCity");
+        return veh;
+    }
+    @Bean
+    Vehicle vehicle3(){
+        var veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+
+    @Bean
+    String hello(){
+        return "hello World";
+    }
+
+    @Bean
+    Integer number(){
+        return 16;
+    }
+}
