368d33739fc2fea99db1f68f30bada6833fffb00
Amkaansuh1993 <amkaansuh.ind@gmail.com>
2025-03-27 00:34:16 +0530

    Update Mar27

---
+++ Spring/example11 - Wiring Bean using Method Parameter/src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import org.example.beans.Person;
+import org.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Vehicle vehicle() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda");
+        return vehicle;
+    }
+
+    @Bean
+    public Person person(Vehicle vehicle) {
+        Person  person = new Person();
+        person.setName("John");
+        person.setVehicle(vehicle);
+        return person;
+    }
+}
