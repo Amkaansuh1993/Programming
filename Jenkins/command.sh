a24754cc585dab9638de134840b1d211fa676349
Amkaansuh1993 <110151854+Amkaansuh1993@users.noreply.github.com>
2025-10-28 23:14:25 +0530

    Convert command.sh to Jenkins pipeline script

--- Jenkins/command.sh
+++ Jenkins/command.sh
@@ -1,1 +1,20 @@
-echo "From Github"
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      steps {
+        echo 'Building the project...'
+      }
+    }
+    stage('Test') {
+      steps {
+        echo 'Running tests...'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        echo 'Deploying application...'
+      }
+    }
+  }
+}
